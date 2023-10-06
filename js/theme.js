2977a6412a0fb74cb3ccbc3ec46c409d6faa5e8a
Ryan Biondo <biondoryan.dev@gmail.com>
2023-10-06 09:24:13 -0600

    fix nav and theme bugs

--- js/theme.js
+++ js/theme.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Initially set the mode based on user's system preference
-  if (
-    window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-  ) {
-    document.body.classList.add('dark-mode');
-  }
-
+  const prefersDarkScheme = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
   const darkModeToggle = document.getElementById('darkModeToggle');
+
+  // Initially set the mode and toggle state based on user's system preference
+  if (prefersDarkScheme) {
+    document.body.classList.add('dark-mode');
+    darkModeToggle.checked = true; // Set the toggle switch to on
+  }
 
   darkModeToggle.addEventListener('change', function () {
     if (this.checked) {

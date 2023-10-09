907a53cc65244ad2660ede4b0b65458f04bbad54
Ryan Biondo <biondoryan.dev@gmail.com>
2023-10-09 01:06:19 -0600

    fine tuned highlight sections

--- js/nav.js
+++ js/nav.js
@@ -1,44 +1,62 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
   let isMobileNavOpen = false;
   const menuIcon = document.getElementById('menuIcon');
   const xIcon = document.getElementById('xIcon');
   const navLinks = document.querySelector('.navLinks');
   const mobileNavLinks = document.querySelector('.mobileNavLinks');
 
-  const toggleMobileNav = () => {
-    mobileNavLinks.classList.toggle('active', true);
+  // Function to toggle the mobile dropdown menu
+  function toggleMobileNav() {
+    mobileNavLinks.classList.add('active');
+    mobileNavLinks.style.display = 'flex'; // Ensure mobileNavLinks is displayed
     xIcon.style.display = 'block';
     menuIcon.style.display = 'none';
-  };
+  }
 
-  const toggleBackToMenu = () => {
-    mobileNavLinks.classList.toggle('active', false);
+  // Function to toggle back to the menu icon
+  function toggleBackToMenu() {
+    mobileNavLinks.classList.remove('active');
+    mobileNavLinks.style.display = 'none'; // Ensure mobileNavLinks is hidden
     xIcon.style.display = 'none';
     menuIcon.style.display = 'block';
-  };
+  }
 
+  // Add click event listeners to the menu icon and close icon
   menuIcon.addEventListener('click', toggleMobileNav);
   xIcon.addEventListener('click', toggleBackToMenu);
 
-  const handleResize = () => {
-    const isMobile = window.innerWidth <= 768;
-    navLinks.style.display = isMobile ? 'none' : 'flex';
-    menuIcon.style.display = isMobile ? 'block' : 'none';
-    xIcon.style.display = 'none';
-    mobileNavLinks.style.display =
-      isMobileNavOpen && isMobile ? 'flex' : 'none';
-  };
+  // Function to handle screen resize
+  function handleResize() {
+    if (window.innerWidth <= 768) {
+      navLinks.style.display = 'none';
+      menuIcon.style.display = 'block';
+      xIcon.style.display = 'none';
+      if (!isMobileNavOpen) {
+        mobileNavLinks.style.display = 'none';
+      }
+    } else {
+      navLinks.style.display = 'flex';
+      menuIcon.style.display = 'none';
+      mobileNavLinks.style.display = 'none';
+      xIcon.style.display = 'none';
+    }
+  }
 
+  // Add event listener for screen resize
   window.addEventListener('resize', handleResize);
+
+  // Initial call to handle screen size on page load
   handleResize();
 });
 
-window.addEventListener('scroll', () => {
+// Highlight nav item on scroll
+
+window.addEventListener('scroll', function () {
   const sections = ['intro', 'about', 'projects', 'contact'];
   const sectionOffsets = {
-    intro: 500,
-    about: 600,
-    projects: 500,
+    intro: 400,
+    about: 400,
+    projects: 400,
     contact: 400,
   };
 
@@ -47,19 +65,37 @@ window.addEventListener('scroll', () => {
     const position = sectionElement.getBoundingClientRect();
     const offset = sectionOffsets[section] || 0;
 
-    const isActive =
-      position.top <= window.innerHeight - offset && position.bottom >= offset;
-    document
-      .querySelectorAll(
-        `.navLinks a[href="#${section}"], .mobileNavLinks a[href="#${section}"]`
-      )
-      .forEach((navLink) => navLink.classList.toggle('active', isActive));
+    if (
+      position.top <= window.innerHeight - offset &&
+      position.bottom >= offset
+    ) {
+      // Remove active class from all nav links
+      document
+        .querySelectorAll('.navLinks a, .mobileNavLinks a')
+        .forEach((navLink) => {
+          navLink.classList.remove('active');
+        });
+
+      // Add active class to the nav link corresponding to the section in view
+      document
+        .querySelector(
+          `.navLinks a[href="#${section}"], .mobileNavLinks a[href="#${section}"]`
+        )
+        .classList.add('active');
+    }
   });
 });
 
-window.addEventListener('scroll', () => {
-  const element = document.getElementById('nav');
-  const isScrolled = window.scrollY > 100;
-  element.style.visibility = isScrolled ? 'visible' : 'hidden';
-  element.style.opacity = isScrolled ? '1' : '0';
+// JavaScript to hide the arrow on scroll
+window.addEventListener('scroll', function () {
+  var element = document.getElementById('nav');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) {
+    element.style.visibility = 'visible';
+    element.style.opacity = '1';
+  } else {
+    element.style.visibility = 'hidden';
+    element.style.opacity = '0';
+  }
 });
